058247df9dd7ea6c7044860d539e037e8d77af47
amste <jeremy.barneron@gmail.com>
2018-10-12 22:53:31 +0200

    Added some commands to the shell manager

--- easy-cut.sh
+++ easy-cut.sh
@@ -6,6 +6,26 @@ if [[ -z "${TAG}" ]]; then
     TAG="latest"
 fi
 
+if [ $1 = "deploy" ]; then
+    if [ $2 = "local" ]; then
+        cd "infra/deploy/local"
+        TAG=${TAG} docker-compose up
+    else
+        echo "Commad deploy ${2} unknown"
+    fi
+fi
+
+if [ $1 = "clean" ]; then
+    cd services
+    for i in "${services[@]}"
+    do
+        cd $i/
+        make clean
+        cd ..
+    done
+    echo "Cleaning done"
+fi
+
 if [ $1 = "release" ]; then
     cd services
     for i in "${services[@]}"
