7e9e0e15fbce2b3236a6b819c25bca83ced9b5b9
amste <jeremy.barneron@gmail.com>
2018-10-12 22:39:03 +0200

    Create shell script for common ops

---
+++ easy-cut.sh
@@ -0,0 +1,28 @@
+services=( appointment auth barber rating salon user )
+
+if [[ -z "${TAG}" ]]; then
+    TAG="latest"
+fi
+
+if [ $1 = "release" ]; then
+    cd services
+    for i in "${services[@]}"
+    do
+        cd $i/
+        make release SERVICE=$i
+        cd ..
+    done
+    echo "Release of all services done"
+    echo $TAG
+fi
+
+if [ $1 = "build" ]; then
+    cd services
+    for i in "${services[@]}"
+    do
+        cd $i/
+        make build SERVICE=$i TAG="${TAG}"
+        cd ..
+    done
+    echo "Build of all services done"
+fi
